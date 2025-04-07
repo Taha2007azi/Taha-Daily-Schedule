a429a841277ed5fda1dee2311808c82692cbc808
Taha2007azi <tahaanzabi2007@gmail.com>
2025-04-08 00:33:42 +0330

    Update taha_daily_schedule_final.py

--- taha_daily_schedule_final.py
+++ taha_daily_schedule_final.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Taha's Planner", layout="wide")  # باید اولین دستور باشه
+
 st.set_page_config(page_title="Taha's Planner", layout="wide")
 
 # ---------- Style ----------
